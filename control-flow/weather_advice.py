a242ad3640f51f1279b208d8a68ce65939d13adf
Victor Muhita <wangombevictor5@gmail.com>
2025-07-18 13:00:14 +0300

    add .gitignore

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,9 +1,9 @@
-weather_inp = input("What's the weather like today? (sunny/rainy/cold): ")
-if weather_inp == "sunny":
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif weather_inp == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat")
-elif weather_inp == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
