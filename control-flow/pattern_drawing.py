a9076503354869678cbd7470ba6b4268504e810a
Victor Muhita <wangombevictor5@gmail.com>
2025-07-18 14:43:03 +0300

    display stars

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,8 +1,11 @@
 inp = int(input("Enter the size of the pattern: "))
 x = 0
+j = 0
 while x < inp:
-    print( "*" * inp)
-    print(end="")
-    x += 1
+    for s in range(inp):
+        print("*",end="")
+    print("")
+    x+=1
+
 
 
